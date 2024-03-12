b6485b4a566292cacb753b34266ee437d4f12d22
Addysaurus <addisonalmagor@gmail.com>
2024-03-12 08:58:02 -0400

    done

--- error.py
+++ error.py
@@ -1,1 +1,10 @@
-
+import csv, json, re
+with open('logs.json', 'r') as file:
+    data = json.load(file)
+    with open('error_logs.csv', 'w') as csv_file:
+        for i in data:
+            if re.search('ERROR', i['level']):
+                i['level'] = 'ERROR'
+        writer = csv.DictWriter(csv_file, fieldnames=data[0].keys(), delimiter=';')
+        writer.writeheader()
+        writer.writerows([i for i in data if re.search('ERROR', i['level'])])
